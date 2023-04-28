ff6de0647fafcc84cd5d0e88072a70962750a77f
ronanter <ronan1terras@gmail.com>
2023-04-28 12:12:41 +0200

    [FEATURE] monitor A et B

--- MonitorB.java
+++ MonitorB.java
@@ -1,9 +1,30 @@
 import Events.BioHazardEvent;
 
+import java.util.ArrayList;
+
 public class MonitorB implements MonitorListener{
+
+    private ArrayList<BioHazardEvent> listEvent = new ArrayList<BioHazardEvent>();
+
+    public MonitorB() {
+        this.listEvent = new ArrayList<BioHazardEvent>();
+    }
     @Override
     public void hazardReception(BioHazardEvent bioHazard) {
-
+        String type = bioHazard.getType();
+        switch (type){
+            case("fire"):
+                System.out.println("erreur, les moniteurs de type B ne peuvent pas gérer les feux");
+                break;
+            case("radiation"):
+                System.out.println("attention aux cancers");
+                listEvent.add(bioHazard);
+                break;
+            case("gaz"):
+                System.out.println("Ca puire messire");
+                listEvent.add(bioHazard);
+                break;
+        }
     }
 
     @Override
